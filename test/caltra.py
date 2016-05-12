53c496ea56775dafffad37215cf084099c89402d
lsaffin <l.saffin@pgr.reading.ac.uk>
2016-05-12 15:54:05 +0100

    Added simple testing scripts to check things run

---
+++ test/caltra.py
@@ -0,0 +1,18 @@
+import datetime
+import numpy as np
+from lagranto.caltra import caltra
+
+mapping = {datetime.datetime(2009, 11, 30, 0): 'datadir/xjjhq/xjjhqa_036.pp',
+           datetime.datetime(2009, 11, 30, 1): 'datadir/xjjhq/xjjhqa_036.pp'}
+
+trainp = np.zeros([2, 3])
+
+trainp[:, 0] = 360
+trainp[0, 2] = 5000
+trainp[1, 2] = 10000
+
+traout = caltra.caltra(trainp, mapping, tracers=['advection_only_pv',
+                                                 'ertel_potential_vorticity'])
+
+for traj in traout:
+    print traj
