ba1a5d813f81d04d2f66ba192d9a1bce9859103c
Leo Saffin <l.saffin@leeds.ac.uk>
2020-06-29 16:24:40 +0100

    Added test for caltra using preprocessed LagTraj data

--- test/caltra.py
+++ test/caltra.py
@@ -1,28 +1,72 @@
-import datetime
-from lagranto import caltra, geometry
-from scripts import case_studies
+import pytest
 
-# Set up the mapping of times to files:
-datadir = '/home/lsaffin/Documents/meteorology/data/iop5/'
-start_time = datetime.datetime(2011, 11, 28, 12)
-mapping = {start_time: datadir + '20111128_analysis12.nc'}
-for n in range(6, 25, 6):
-    time = start_time + datetime.timedelta(hours=n)
-    mapping[time] = datadir + 'prognostics_' + str(n).zfill(3) + '.nc'
+import numpy as np
 
-# Define the start points
-forecast = case_studies.iop5b.copy()
-cubes = forecast.set_lead_time(hours=24)
-levels = ('air_potential_temperature', [315])
-trainp = geometry.select(cubes, 'total_minus_advection_only_theta', '>', 10,
-                         levels=levels)
-#trainp = geometry.select_inflow_region(datadir + 'backward_trajectories.pkl')
-#trainp = geometry.circle((366, 6.5), 6, [320], 0.5)
+import iris
 
-# Calculate the trajectories
-traout = caltra.caltra(trainp, mapping, fbflag=-1,
-                       tracers=['air_potential_temperature', 'air_pressure'])
+from pylagranto.fortran import caltra
 
-# Save the trajectories
-traout.to_pickle(
-    datadir + 'backward_trajectories_from_heated_region_315K.pkl')
+
+def test_caltra():
+    x, y, z = 302.33, 13.33, 95000.
+    ntra = 1
+    leftflag = np.zeros(1)
+    ts = 900.
+    nsubs = 4
+    imethod = 1
+    numit = 3
+    jflag = False
+    fbflag = -1
+
+    hem = 0
+    per = 0
+
+    cubes0 = iris.load("ERA5_950hPa_2020-02-05T15:00.nc")
+    cubes1 = iris.load("ERA5_950hPa_2020-02-05T14:00.nc")
+
+    xmin, ymin, dx, dy, nx, ny, nz = grid_parameters(cubes0)
+    spt0, p3t0, uut0, vvt0, wwt0 = load_winds(cubes0)
+    spt1, p3t1, uut1, vvt1, wwt1 = load_winds(cubes1)
+
+    x, y, z, leftflag = caltra.main(
+                    x, y, z, leftflag, ts, nsubs, imethod, numit, jflag, fbflag,
+                    spt0, spt1, p3t0, p3t1, uut0, uut1, vvt0, vvt1, wwt0, wwt1,
+                    xmin, ymin, dx, dy, per, hem, nx, ny, nz, ntra)
+
+    assert x == 302.69702148
+    assert y == 13.37627983
+
+
+def grid_parameters(cubes):
+    cube = cubes.extract_strict("northward_wind")
+
+    x = cube.coord("longitude").points
+    y = cube.coord("latitude").points
+
+    # Find minimum latitude and longitude
+    xmin = x.min()
+    ymin = y.min()
+
+    # Grid spacing
+    dx = abs((x[1:] - x[:-1]).mean())
+    dy = abs((y[1:] - y[:-1]).mean())
+
+    # Extract grid dimesions
+    _, nz, ny, nx = cube.shape
+
+    return xmin, ymin, dx, dy, nx, ny, nz
+
+
+def load_winds(cubes):
+    sp = cubes.extract_strict("surface_air_pressure")
+
+    u = cubes.extract_strict("eastward_wind")
+    v = cubes.extract_strict("northward_wind")
+    w = v.copy(data=np.zeros_like(v.data))
+    p = v.copy(data=np.ones_like(v.data)*95000.)
+
+    return [cube.data.transpose().flatten(order="F").data.astype(np.double) for cube in [sp, p, u, v, w]]
+
+
+if __name__=="__main__":
+    test_caltra()
