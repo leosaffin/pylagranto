687cf7f005be85c555399bdd72457ab29e594b41
Leo Saffin <l.saffin@leeds.ac.uk>
2023-11-23 13:00:54 +0000

    Include time coordinate in testdata

--- test/test_caltra.py
+++ test/test_caltra.py
@@ -1,7 +1,5 @@
 import pytest
 
-import datetime
-
 import numpy as np
 
 from pylagranto import caltra
@@ -9,17 +7,10 @@ from pylagranto import caltra
 
 @pytest.mark.parametrize("imethod", [1, 2])
 def test_caltra(testdata, imethod):
-    # Set up the mapping of times to files:
-    start_time = datetime.datetime(2020, 1, 1)
-    dt = datetime.timedelta(hours=1)
-    mapping = {start_time + n*dt: str(testdata) for n in range(2)}
-
     trainp = np.array([[180, 0, 5000]])
 
-    print(trainp)
-
     # Calculate the trajectories
-    traout = caltra.caltra(trainp, mapping, imethod=imethod)
+    traout = caltra.caltra(trainp, testdata, imethod=imethod)
 
     assert traout.x[0, 1] == pytest.approx(180.0323, abs=0.001)
     assert traout.y[0, 1] == pytest.approx(0.0323, abs=0.001)
